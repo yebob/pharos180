6ac789046a5fe18147a119e3c7a8686635194c0d
Rodrigo Matos <rmatos@bionexo.com>
2014-05-24 15:57:48 -0300

    create model Profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524181258) do
+ActiveRecord::Schema.define(version: 20140524184656) do
+
+  create_table "profiles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
