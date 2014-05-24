e3cfb56602ac1d72ec0b7de536f0f029db33d61a
Rodrigo Matos <rmatos@bionexo.com>
2014-05-24 15:43:36 -0300

    create model role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524170438) do
+ActiveRecord::Schema.define(version: 20140524181258) do
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "skill_categories", force: true do |t|
     t.string   "name"
