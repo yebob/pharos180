cb7f7103d9ee131e25479b922015aed506fd230d
Gabriel Lett Viviani <gm.lett@gmail.com>
2014-05-27 15:14:48 -0300

    Created Levels and Specializations views, controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   resources :areas 
   resources :profiles
   resources :roles
-
-
+  resources :specializations
+  resources :levels
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
