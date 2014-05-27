a28a45ab610acb639c53e865465b4096cbd78bb1
Israel Pinto Viegas <israelpintoviegas@MacBook-Pro-de-Israel.local>
2014-05-27 14:23:15 -0300

    Create routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,55 +3,11 @@ Rails.application.routes.draw do
 
   resources :skills
   resources :positions
-  # get '/positions(.:format)', to: 'positions#index', as: :position
-  # post '/positions(.:format)', to: 'positions#create'
-  # get '/positions/new(.:format)', to: 'positions#new', as: :new_position
-  # get '/positions/:id/edit(.:format)', to: 'positions#edit', as: :edit_position 
-  # get '/positions/:id(.:format)', to: 'positions#show', as: :positions
-  # patch '/positions/:id(.:format)', to: 'positions#update'
-  # put '/positions/:id(.:format)', to: 'positions#update'
-  # delete '/positions/:id(.:format)', to: 'positions#destroy'
+  resources :users
+  resources :areas 
+  resoucers :profiles
+  resources :roles
 
-
-  #resources :users
-  #configuracao das rotas para Users
-  get '/usuarios', to:'users#index',as: :users
-  get '/usuarios/novo', to: 'users#new', as: :new_user
-  post '/usuarios', to:'users#create'
-  delete '/usuarios/apagar', to:'skills#destroy'
-  get '/users/:id', to: 'users#show', as: :user
-  put '/users/:id', to: 'users#update'
-  get '/users/:id/edit', to: 'users#edit', as: :edit_user
-
-
-  get '/areas(.:format)', to: 'areas#index', as: :areas
-  post '/areas(.:format)', to: 'areas#create'
-  get '/areas/new(.:format)', to: 'areas#new', as: :new_area
-  get '/areas/:id/edit(.:format)', to: 'areas#edit', as: :edit_area
-  get '/areas/:id(.:format)', to: 'areas#show', as: :area
-  patch '/areas/:id(.:format)', to: 'areas#update'
-  put '/areas/:id(.:format)', to: 'areas#update'
-  delete '/areas/:id(.:format)', to: 'areas#destroy'
-
-   #configuracao das rotas para profiles
-  get '/profiles(.:format)', to: 'profiles#index', as: :profile
-  post   '/profiles(.:format)', to: 'profiles#create', as: :profiles
-  get    '/profiles/new(.:format)', to: 'profiles#new', as: :new_profile
-  get    '/profiles/:id/edit(.:format)', to: 'profiles#edit', as: :edit_profile
-  get    '/profiles/:id(.:format)', to: 'profiles#show', as: :show_profile
-  patch '/profiles/:id(.:format)', to: 'profiles#update', as: :update_profile1
-  put    '/profiles/:id(.:format)', to: 'profiles#update', as: :update_profile2
-  delete '/profiles/:id(.:format)', to: 'profiles#destroy', as: :delete_profile
-
-  #configuracao das rotas para roles
-  get 'roles' => 'roles#index', as: :roles
-  get 'roles/new' => 'roles#new', as: :new_role
-  get 'roles/:id/edit' => 'roles#edit', as: :edit_role
-  get 'roles/:id' => 'roles#show', as: :role
-  post 'roles' => 'roles#create'
-  delete 'roles/:id' => 'roles#destroy'
-  put 'roles/:id' => 'roles#update'
-  patch 'roles/:id' => 'roles#update'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
