4bd080de8af463cf219fa133b1ef1e2ea6cfd165
Wellington Santos <wjsantos.wj@gmail.com>
2014-07-04 15:18:12 -0300

    organizing code and associate skill with many skill_levels

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,9 +1,12 @@
 class Skill < ActiveRecord::Base
-  validates :name, presence: true, allow_blank: false, length: { minimum: 5 }
-  validates :skill_category, presence: true, allow_blank: false
+  belongs_to :skill_category
+
   has_many :skill_relevances
-  has_many :specializations, through: :skill_relevances  
+  has_many :specializations, through: :skill_relevances
   has_many :proficiency_levels
   has_many :levels, through: :proficiency_levels
-  belongs_to :skill_category
+  has_many :skill_leves
+
+  validates :name, presence: true, allow_blank: false, length: { minimum: 5 }
+  validates :skill_category, presence: true, allow_blank: false
 end
