56119ccadf3ab9b520288d5d0913ff747455704c
Viviane França <vivianefranca@vivi-2.local>
2014-07-19 23:18:10 -0300

    Revert "removing duplicates add column in PositionSkill"
    This reverts commit 44de419113f472c01d756b7b5a0a1f60ca35b64f.

--- db/migrate/20140719211722_create_position_skills.rb
+++ db/migrate/20140719211722_create_position_skills.rb
@@ -3,7 +3,6 @@ class CreatePositionSkills < ActiveRecord::Migration
     create_table :position_skills do |t|
       t.references :position, index: true
       t.references :level, index: true
-      t.references :skill, index: true
 
       t.timestamps
     end
