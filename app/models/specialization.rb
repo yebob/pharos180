e0624c6fd0c56a00a42b0ef45565889b21d5bc67
Viviane França <vivianefranca@vivi.spo.virtua.com.br>
2014-07-20 15:54:45 -0300

    Adding references in models

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -1,9 +1,10 @@
 class Specialization < ActiveRecord::Base
 
-  belongs_to :specialization_catefory
+  belongs_to :specialization_category
 
   has_many :specialization_skills
   has_many :specialization_weights
+  has_many :positions
 
   validates :name, presence: true, allow_blank: false, length: { minimum: 5 }
 
