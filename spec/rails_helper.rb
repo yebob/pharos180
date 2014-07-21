e1de2acb0964abb24b768395841bdcb8f67c4dae
Wellington Santos <wjsantos.wj@gmail.com>
2014-07-20 21:53:02 -0300

    adding database_cleaner into rails_helper to clean the database before each spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,3 +44,17 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
+
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
