be1918c9decea555301a5a48db36eeda33e63b6e
Wellington Santos <wjsantos.wj@gmail.com>
2014-07-21 21:05:11 -0300

    adding specs for position  model

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -2,4 +2,13 @@ require "rails_helper"
 
 describe Position do
   it { should belong_to(:area) }
+  it { should belong_to(:specialization) }
+
+  it { should have_many(:users) }
+  it { should have_many(:position_skills) }
+  it { should have_many(:skills).through(:position_skills) }
+
+  it { should allow_value('foobar').for(:name) }
+  it { should_not allow_value('foo').for(:name) }
+  it { should_not allow_value('').for(:name) }
 end
