e27125a361fb337cc0c53e5a655ab0be00e3b07e
Wellington Santos <wjsantos.wj@gmail.com>
2014-07-22 21:06:57 -0300

    position validations for presence of specialization and area

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -2,7 +2,12 @@ require "rails_helper"
 
 describe Position do
   it { should belong_to(:area) }
+  it { should allow_value(build(:technology)).for(:area) }
+  it { should_not allow_value(nil).for(:area) }
+
   it { should belong_to(:specialization) }
+  it { should allow_value(build(:maker)).for(:specialization) }
+  it { should_not allow_value(nil).for(:specialization) }
 
   it { should have_many(:users) }
   it { should have_many(:position_skills) }
