7269e5a227ed503fb16873b3bbe6b8e4b45e3db7
Wellington Santos <wjsantos@bionexo.com>
2014-07-25 18:37:06 -0300

    new factories skill_category

--- spec/factories/skill_category.rb
+++ spec/factories/skill_category.rb
@@ -3,5 +3,11 @@ FactoryGirl.define do
     factory :corporate do
       name "Corporate"
     end
+    factory :management do
+      name "Management"
+    end
+    factory :specific do
+      name "Specific"
+    end
   end
 end
